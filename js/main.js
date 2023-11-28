1a7d183d3a928375a7dde2363d78031867e284cd
spenserg92 <spenserg92@gmail.com>
2023-11-28 16:30:24 -0500

    adds functions

--- js/main.js
+++ js/main.js
@@ -15,7 +15,8 @@
 // want to add some Audio to the countdown to improve user experience
 
 /*------ constants ------*/
-
+// our audo file to play during countdown
+const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
 // list of choices
 const RPS_LOOKUP = {
     r: {img: 'imgs/rock.png', beats: 's'},
@@ -38,5 +39,125 @@ let results
 let winner
 
 /*------ functions ------*/
+// initializer functin -> set up our intial state and call render
+function init(){
+    scores = {
+        p: 0,
+        c: 0,
+        t: 0,
+    }
+    results = {
+        p: 'r',
+        c: 'r',
+    }
+
+    winner = 't'
+    
+    render()
+    // eventually call render() when render has been built
+}
+
+init()
+// renderScores -> show how many wins/ losses/ ties
+function renderScores(){
+
+    for (let key in scores){
+        const scoreEl = document.getElementById(`${key}-score`)
+        scoreEl.innerText = scores[key]
+    }
+}
+// renderResults -> show the results of player and computer choices
+function renderResults(){
+    pResultEl.src = RPS_LOOKUP[results.p].img
+    cResultEl.src = RPS_LOOKUP[results.c].img
+
+    pResultEl.style.borderColor = winner === 'p' ? 'purple' : 'white'
+    cResultEl.style.borderColor = winner === 'c' ? 'purple' : 'white'
+}
+
+// render countdown
+
+function renderCountdown(cbFunc){
+    //start count at 3
+    let count = 3
+    // display countdown div and set the text
+    countdownEl.style.visibility = 'visible'
+    countdownEl.innerText = count
+    //timer will update dom every second
+    //once timer is up, display results
+
+    AUDIO.currentTime = 0
+    AUDIO.play()
+
+    // set up timer
+    // set interval
+    const timerId = setInterval(() => {
+        // every second decrease count
+        count--
+        //if count is truthy do something
+        if(count) {
+            console.log('interval running. count : ', count)
+            countdownEl.innerText = count
+        } else {
+            clearInterval(timerId)
+            // once
+            countdownEl.style.visibility = 'hidden'
+
+            //when timer is done call callbackfunction
+            cbFunc()
+        }
+    }, 1000)
+}
+
+// render -> transfer/visualize all changes to the dom
+function render(){
+    renderCountdown(() => {
+        renderScores()
+        renderResults()
+    })
+
+}
+
+// we'll do this by calling a couple other render functions through a countdown
+
+// getRandom function for our computer player to select a move
+function getRandomRPS (){
+    const rps = Object.keys(RPS_LOOKUP)
+    console.log('this is rps inside getRandomRPS', rps)
+
+
+    const randomIndex = Math.floor(Math.random() * rps.lenght)
+    console.log('random index inside getRandomRPS', randomIndex)
+
+    return rps[randomIndex]
+    // need a getWinner function -> determine who wins
+
+}
+
+function getWinner (){
+    // compare our results object keys and base the output of this function
+    // on what
+    if (results.p === results.c){ return 't'}
+
+    return RPS_LOOKUP[results.p].beats === results.c ? 'p' : 'c'
+}
+
+// handleChoice -> for the player to select a move (event Listener)
+// we'll use the innerText of our event target to determine what move is
+function handleChoice(evt){
+    // handle when the user clicks something that is not a button
+    if (evt.target.tagName !== 'BUTTON'){return}
+    // change results.p to whatever is selected using innerText
+    results.p = evt.target.innerText.toLowerCase()
+    // call the randome selector for our computer player
+    results.c = getRandomRPS()
+    // check for a winner
+    winner = getWinner()
+    console.log('this is the winner', winner)
+    // update scores
+    scores[winner] += 1
+
+    render()
+}
 
 /*------ event listeners ------*/
